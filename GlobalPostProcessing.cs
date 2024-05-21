66138d95cfdaf4d6fcaf068c55dfb4e79c1aefac
itsKat <77287432+itsKatVR@users.noreply.github.com>
2024-05-21 19:22:05 -0400

    Add files via upload

---
+++ GlobalPostProcessing.cs
@@ -0,0 +1,28 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using UnityEngine.UI;
+using UnityEngine.Rendering.PostProcessing;
+
+public class GlobalPostProcessing : UdonSharpBehaviour
+{
+    public Slider volumeSlider;
+    public PostProcessVolume volume;
+    [UdonSynced(UdonSyncMode.Smooth)] public float syncedValue;
+
+    void Start() { volumeSlider.SetValueWithoutNotify(volume.weight); }
+    public override void OnDeserialization()
+    {
+        volumeSlider.SetValueWithoutNotify(syncedValue);
+        volume.weight = syncedValue;
+    }
+    public void SetVolume()
+    {
+        Networking.SetOwner(Networking.LocalPlayer, gameObject);
+        syncedValue = volumeSlider.value;
+        volume.weight = syncedValue;
+        RequestSerialization();
+    }
+}
